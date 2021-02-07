9317a2ea41740e41e871e425b4215b3145580a20
Ennin <enninfrancis47@gmail.com>
2021-02-07 12:48:06 +0000

    make it better

--- carts/admin.py
+++ carts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Order, OrderItem, OrderedItem
 
-admin.site.register(OrderItem)
-admin.site.register(Order)
+# admin.site.register(OrderItem)
+# admin.site.register(Order)
 admin.site.register(OrderedItem)
 
